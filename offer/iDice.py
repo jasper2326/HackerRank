7497b9d2e542741cd7472f794ea14985013870e5
jasper2326 <jasper_jiao@hotmail.com>
2018-03-01 15:27:24 +0800

    iDice update push

--- offer/iDice.py
+++ offer/iDice.py
@@ -1,51 +1,60 @@
-import numpy as np
-import pandas as pd
+import math
 
 
-
-def thread():
-    data = dataLoader()
+def thread(iEThreshold, cDThreshold, rPThreshold):
+    # load data
+    data, elements = dataLoader()
+    # get posible feature combinations
     candidates = getCandidates()
 
     result = []
     final = []
-    iEThreshold, cDThreshold, rPThreshold = 0, 0, 0
 
 
     while candidates:
-        p = candidates.pop()
+        p = candidates.pop(0)
+        # validation
         if not p or len(p) == 0:
             break
-
-        iE = impackEvaluation(p)
+        # get all subsets of p
+        subsets = getSubstes(elements, p)
+        # calculate impact evaluation of p
+        iE = impactEvaluation(data, elements, p)
         if iE < iEThreshold:
-            for item in getSubstes(p):
-                candidates.remove(item)
+            # remove all p's subsets
+            for subset in subsets:
+                if subset in candidates:
+                    candidates.remove(subset)
             continue
-
-        cD = changeDection(p)
+        # calculate change rate
+        cD = changeDection(data, elements, p)
         if cD < cDThreshold:
             continue
-
-
-        iP = isolationPower(p)
+        # calculate isolation power of p and its subsets
+        iP = isolationPower(data, elements, p)
         iPs = []
-        for subset in getSubstes(p):
-            iPs.append(isolationPower(subset))
+        for subset in subsets:
+            iPs.append(isolationPower(data, elements, subset))
+        # check if p's ip is greater than all its subsets
         if iP > max(iPs):
             result.append(p)
-        else:
-            for item in getSubstes(p):
-                candidates.remove(item)
-
+            for item in getSubstes(elements, p):
+                if item in candidates:
+                    candidates.remove(item)
+    print(len(result))
     for p in result:
-        rP = getRP(p)
-        if rP < rPThreshold:
-            result.remove(p)
-        else:
+        rP = getRP(data, elements, p)
+        if rP > rPThreshold:
             final.append([p, rP])
 
-    return final
+
+    def count0(word1):
+        length = 0
+        for char in word1:
+            if char == '0':
+                length += 1
+        return length
+    return sorted(final, key=count0)
 
 
 
@@ -53,35 +62,113 @@ def thread():
 
 
 def dataLoader():
-    data = []
+    data = {}
+    elements = set()
     file = open("data.txt", "r")
-    for line in file.readlines():
-        data.append(line.strip().split(','))
-    return data[1:]
+
+    for lines in file.readlines():
+        line = lines.strip().split(',')
+        # features are stored as a tuple
+        key = tuple(line[0:4])
+        # values are stored as a list
+        if key in elements:
+            data[key].append([int(line[-2]), int(line[-1])])
+        else:
+            elements.add(key)
+            data[key] = [[int(line[-2]), int(line[-1])]]
+
+
+    return data, elements
+
 
 def getCandidates():
-    pass
+    import itertools
+    result = []
+
+    for item in itertools.permutations('0000aaaabbbbcccc', 4):
+        result.append(item)
+
+    def count0(word1):
+        length = 0
+        for char in word1:
+            if char == '0':
+                length += 1
+        return length
+
+    return sorted(set(result), key=count0, reverse=True)[1:]
 
 
-def impackEvaluation(p):
-    pass
+def evaluate(source, target):
+    for i in range(len(source)):
+        if source[i] == '0':
+            continue
+        elif source[i] != target[i]:
+            return False
+    return True
 
 
-def getSubstes(p):
-    pass
+def getSubstes(elements, p):
+    result = []
+    for element in elements:
+        if evaluate(p, element):
+            result.append(element)
+    return result
 
 
-def changeDection(p):
-    pass
+def impactEvaluation(data, elements, p):
+    ie = 0
+    for element in getSubstes(elements, p):
+        ie += data[element][0][0]
+    return ie
 
 
-def isolationPower(p):
-    pass
+def changeDection(data, elements, p):
+    cd = 0
+    for element in getSubstes(elements, p):
+        cd += abs(data[element][0][1] - data[element][0][0])
+    return cd
 
 
-def getRP(p):
-    pass
+def isolationPower(data, elements, p):
+    ip = 0
+    Xa, Xb, Oa, Ob = 0, 0, 0, 0
+    for element in elements:
+        Oa += data[element][0][1]
+        Ob += data[element][0][0]
+        if evaluate(p, element):
+            Xa += data[element][0][1]
+            Xb += data[element][0][0]
+    pax, pbx = Xa/(Xa + Xb), Xb/(Xa + Xb)
+    paxbar, pbxbar = (Oa - Xa)/(Oa + Ob - Xa - Xb), (Ob - Xb)/(Oa + Ob - Xa - Xb)
+    return -(Xa * math.log2(1/pax) + Xb * math.log2(1/pbx)
+             + (Oa - Xa) * math.log2(1/paxbar) + (Ob - Xb) * math.log2(1/pbxbar))/(Oa + Ob)
 
 
 
-print(dataLoader())
+def getRP(data, elements, p):
+    Xa, Xb, Oa, Ob = 0, 0, 0, 0
+    for element in elements:
+        Oa += data[element][0][1]
+        Ob += data[element][0][0]
+        if evaluate(p, element):
+            Xa += data[element][0][1]
+            Xb += data[element][0][0]
+    pa, pb = Xa/Oa, Xb/Ob
+    return pa * math.log2(pa/pb)
+
+
+data, elements = dataLoader()
+#print(data)
+#print(elements)
+cans = getCandidates()
+#print(type(cans))
+#print(cans)
+#print(evaluate(('c', '0', '0', '0'), ('c', 'a', '0', '0')))
+#print(getSubstes(elements, ('0', 'a', 'b', 'c')))
+#print(impackEvaluation(data, elements, ('c', 'a', '0', '0')))
+#print(changeDection(data, elements, ('c', 'a', '0', '0')))
+#print(isolationPower(data, elements, ('c', 'a', '0', '0')))
+#print(getRP(data, elements, ('a', '0', '0', '0')))
+
+decide = sorted(thread(300, 100, 0.1), key=lambda x: x[1], reverse=True)
+print(decide)
